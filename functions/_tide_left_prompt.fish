cc2754bfdac2ee939b9eb3befa05bc12a8c5de0f
Ilan Cosman <ilancosman@gmail.com>
2020-06-21 16:49:31 -0700

    Makes newlines work well, add prefixes, suffixes to lines

--- functions/_tide_left_prompt.fish
+++ functions/_tide_left_prompt.fish
@@ -2,12 +2,21 @@ function _tide_left_prompt
     set -l splitText (_fetch_left_prompt_items | string split '@NEWLINE@')
 
     for text in $splitText
-        printf '%s\n' $text
+        string replace "$tide_left_prompt_item_separator" "$tide_left_prompt_prefix" "$text$tide_left_prompt_suffix"
     end
 end
 
 function _fetch_left_prompt_items
-    for item in _tide_item_{$tide_left_prompt_items}
-        printf '%s' ($item)$_tide_color_normal
+    for item in $tide_left_prompt_items
+        set -l output (_tide_item_$item)
+        set -l colorName 'tide_'$item'_bg_color'
+
+        set_color -b $$colorName
+
+        if test "$item" = 'newline'
+            printf '%s' {$output}
+        else
+            printf '%s' $tide_left_prompt_item_separator{$output}
+        end
     end
 end
